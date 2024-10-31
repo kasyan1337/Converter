65d4efac21c6578b487d0b04483a7ad4224a3465
kasyan1337 <tristokorun@gmail.com>
2024-10-31 19:24:18 +0100

    storing project on github

--- src/image_converter.py
+++ src/image_converter.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import pyheif
-from pathlib import Path
+
 
 def heic_to_jpg(input_path: str, output_path: str):
     heif_file = pyheif.read(input_path)
@@ -12,12 +12,24 @@ def heic_to_jpg(input_path: str, output_path: str):
         heif_file.mode,
         heif_file.stride,
     )
-    image.save(output_path, "JPEG")
+    image.save(output_path, "JPEG", quality=100)  # Set quality to maximum
 
-def jpg_to_png(input_path: str, output_path: str):
-    image = Image.open(input_path)
+
+def jpg_to_png(input_path: str, output_path: str, transparent_color=None):
+    image = Image.open(input_path).convert("RGBA")
+    if transparent_color:
+        datas = image.getdata()
+        new_data = []
+        for item in datas:
+            # Make specified color transparent
+            if item[:3] == transparent_color:
+                new_data.append((255, 255, 255, 0))  # to transparent
+            else:
+                new_data.append(item)
+        image.putdata(new_data)
     image.save(output_path, "PNG")
 
+
 def png_to_jpg(input_path: str, output_path: str):
-    image = Image.open(input_path)
-    image.convert("RGB").save(output_path, "JPEG")
+    image = Image.open(input_path).convert("RGB")
+    image.save(output_path, "JPEG", quality=100)  # Set quality to maximum
--- src/video_converter.py
+++ src/video_converter.py
@@ -1,10 +1,37 @@
+from moviepy.editor import VideoFileClip, ImageSequenceClip
 
-from moviepy.editor import VideoFileClip
 
 def mov_to_mp4(input_path: str, output_path: str):
     clip = VideoFileClip(input_path)
-    clip.write_videofile(output_path)
+    clip.write_videofile(
+        output_path, codec="libx264", preset="slow", ffmpeg_params=["-crf", "18"]
+    )  # CRF 18 for near-lossless
+
 
 def mp4_to_mov(input_path: str, output_path: str):
     clip = VideoFileClip(input_path)
-    clip.write_videofile(output_path, codec='libx264')
+    clip.write_videofile(
+        output_path, codec="prores", ffmpeg_params=["-qscale", "0"]
+    )  # Best quality
+
+
+def video_to_gif(input_path: str, output_path: str):
+    clip = VideoFileClip(input_path)
+    clip.write_gif(output_path, fps=10)
+
+
+def gif_to_video(input_path: str, output_path: str):
+    clip = VideoFileClip(input_path)
+    clip.write_videofile(
+        output_path, codec="libx264", preset="slow", ffmpeg_params=["-crf", "18"]
+    )
+
+
+def live_photo_to_gif(input_path: str, output_path: str):
+    clip = VideoFileClip(input_path)
+    clip.write_gif(output_path, fps=10)  # Save as GIF
+
+
+def live_photo_to_video(input_path: str, output_path: str):
+    clip = VideoFileClip(input_path)
+    clip.write_videofile(output_path, codec="libx264")
